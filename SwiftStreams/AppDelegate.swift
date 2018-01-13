89f053f77c52cd260bdf4c9e52f8447d00f99de8
Christophe Maldivi <christophe.maldivi@orange.com>
2018-01-13 22:39:05 +0100

    test(streams): add first filter, map, reduce tests

--- SwiftStreams/AppDelegate.swift
+++ SwiftStreams/AppDelegate.swift
@@ -2,8 +2,4 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-    }
-    func applicationWillTerminate(_ aNotification: Notification) {
-    }
 }
--- SwiftStreamsTests/SwiftStreamsTests.swift
+++ SwiftStreamsTests/SwiftStreamsTests.swift
@@ -3,26 +3,43 @@ import XCTest
 
 class SwiftStreamsTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    class Food {
+        let name: String
+        let glutenFree: Bool
+        let price: Double
+        
+        init(name: String, glutenFree: Bool, price: Double) {
+            self.name = name
+            self.glutenFree = glutenFree
+            self.price = price
         }
     }
     
+    func test_filtering_range() {
+        // given
+        let integers: CountableRange<Int> = 0..<10
+        
+        // when
+        let even: [Int] = integers.filter({$0 % 2 == 0})
+        
+        // then
+        XCTAssertEqual(even, [0, 2, 4, 6, 8])
+    }
+    
+    func test_filter_gluten_free_then_sum_items_price() {
+        // given
+        let foods = [ Food(name: "pancakes", glutenFree: false, price: 2.0),
+                      Food(name: "buckwheat pancakes", glutenFree: true, price: 3.0),
+                      Food(name: "eggs", glutenFree: true, price: 1.0),
+                      Food(name: "toast", glutenFree: false, price: 1.0),
+                      Food(name: "muffins", glutenFree: false, price: 3.0) ]
+        
+        // when
+        let sum = foods.filter({$0.glutenFree})
+                       .map({$0.price})
+                       .reduce(0, +)
+        
+        // then
+        XCTAssertEqual(sum, 4)
+    }
 }
